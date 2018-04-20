bf67923c33f5ec6b46494ab12fd9286b35b6677a
DaniilSmirnov <littlebigcrafting@gmail.com>
2018-04-20 14:04:38 +0300

    переделан внешний вид

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import numexpr as ne
 from random import *
 import time
 
-
 class Ui_MainWindow(object):
 
     i = 0
@@ -22,43 +21,42 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
 
         self.grid = QGridLayout()
-        self.grid.setSpacing(10)
+        self.grid.setSpacing(1)
         self.centralwidget.setLayout(self.grid)
 
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName("label")
-        self.grid.addWidget(self.label,1,0)
+        self.grid.addWidget(self.label,2,0)
 
         self.delta = QLabel(self.centralwidget)
         self.delta.setObjectName("label")
-        self.grid.addWidget(self.delta,1,1)
-
+        self.grid.addWidget(self.delta,2,1)
 
         self.function_enter = QLineEdit(self.centralwidget)
         self.function_enter.setObjectName("lineEdit")
-        self.grid.addWidget(self.function_enter,2,0)
+        self.grid.addWidget(self.function_enter,3,0)
 
         self.point_enter = QLineEdit(self.centralwidget)
         self.point_enter.setObjectName("lineEdit")
-        self.grid.addWidget(self.point_enter,2,1)
+        self.grid.addWidget(self.point_enter,4,0)
 
 
         self.epsilon_enter = QLineEdit(self.centralwidget)
         self.epsilon_enter.setObjectName("lineEdit")
-        self.grid.addWidget(self.epsilon_enter, 2, 2)
+        self.grid.addWidget(self.epsilon_enter, 5, 0)
 
         self.draw_epsilon_button = QPushButton(self.centralwidget)
         self.draw_epsilon_button.setObjectName("pushButton")
-        self.grid.addWidget(self.draw_epsilon_button, 3, 1)
+        self.grid.addWidget(self.draw_epsilon_button, 5, 1)
 
         self.clean_all_button = QPushButton(self.centralwidget)
         self.clean_all_button.setObjectName("pushButton")
-        self.grid.addWidget(self.clean_all_button, 3, 2)
+        self.grid.addWidget(self.clean_all_button, 4, 1)
 
 
         self.graphicsView = pg.PlotWidget(self.centralwidget)
         self.graphicsView.setObjectName("graphicsView")
-        self.grid.addWidget(self.graphicsView,4, 0,5,0)
+        self.grid.addWidget(self.graphicsView,6, 0,7,0)
 
         #self.standart_plot()
 
@@ -70,11 +68,9 @@ class Ui_MainWindow(object):
 
         self.limits_view = self.menubar.addMenu('Limits')
         MainWindow.setMenuBar(self.menubar)
-        self.limits_view.addAction(self.open_limits())
 
         self.derivatives_view = self.menubar.addMenu('Derivatives')
         MainWindow.setMenuBar(self.menubar)
-        self.derivatives_view.addAction(self.open_derivatives())
 
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -184,6 +180,7 @@ class Ui_MainWindow(object):
         function = self.function_enter.text()
         point = self.point_enter.text()
         e = float(self.epsilon_enter.text())
+        self.delta.setText(" ")
 
         Ex = []
         Ey = []
